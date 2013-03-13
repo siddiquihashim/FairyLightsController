6ee0863bad32be3e2cde4e37f629697920fcf64d
Basit Siddiqui <siddiquihashim@gmail.com>
2013-03-13 10:18:33 +0000

    formatted

--- 12FairyLightsController/src/main/java/com/unisys/fairylights/controller/FairyLightsController.java
+++ 12FairyLightsController/src/main/java/com/unisys/fairylights/controller/FairyLightsController.java
@@ -6,22 +6,25 @@ import com.unisys.fairylights.strategy.DisplayStrategyFactory;
 
 /**
  * FairyLightsController responsible for displaying fairy lights as per the {@link DisplayStrategy}.
+ * 
  * @see DisplayStrategy
  */
 public class FairyLightsController {
 
 	/**
 	 * Execute the given strategy for displaying fairy lights
-	 * @param displayStrategyName the name of the strategy
+	 * 
+	 * @param displayStrategyName
+	 *            the name of the strategy
 	 */
 	public void run(String displayStrategyName, int numberOfLights) {
 		run(DisplayStrategyFactory.getStrategy(displayStrategyName), numberOfLights);
 	}
-	
 
 	/**
 	 * @param displayStrategy
-	 * @param numberOfLights length of fairy lights
+	 * @param numberOfLights
+	 *            length of fairy lights
 	 */
 	public void run(DisplayStrategy displayStrategy, int numberOfLights) {
 		displayStrategy.execute(FairyLightsFactory.buildFairyLights(numberOfLights));
